23c3b1cf5fc24bddb6acdab5f71d68432b586a65
iiTONELOC <anthonytropeano@me.com>
2021-01-20 01:12:25 -0500

    added UI/UX improvements and custom styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,7 +80,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -212,16 +212,24 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
+    $(this).addClass("dropover");
     console.log("activate", this);
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
     console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
     console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event) {
     console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   update: function(event) {
    // array to store the task data in
@@ -292,6 +300,14 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+// automatic task auditing
+setInterval(function(){
+  $(".card .list-group-item").each(function(index,el){
+    auditTask(el);
+  });
+
+}, 1800000);
 
 
 
